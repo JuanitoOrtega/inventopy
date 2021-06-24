e4c8c8a6796a13715201cf012db00ca3f8fd48d6
Juanito Ortega <ortegaj83@gmail.com>
2021-06-24 18:15:09 -0400

    Django Secret Key protected

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,9 @@ import os
 
 import config.db as db
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v0rz+av-pt=yxay2w-x7to-#vyu*z!ci3_6u73(@0-u#rcd5^!'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

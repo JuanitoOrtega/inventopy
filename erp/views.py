5344fbf5d49e86d04a6d3fe8fe39df68041588bd
Juanito Ortega <ortegaj83@gmail.com>
2021-06-24 21:46:56 -0400

    Templates part 1

--- erp/views.py
+++ erp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def myfirstview(request):
+    data = {
+        'name': 'Juanito Ortega'
+    }
+    return render(request, 'index.html', data)

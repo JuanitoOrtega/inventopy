97e1ee86c3f2da708582c1715794d1f6f65ade8a
Juanito Ortega <ortegaj83@gmail.com>
2021-06-28 23:51:45 -0400

    Sobreescritura del método post

--- erp/views/category/views.py
+++ erp/views/category/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
-
+from django.utils.decorators import method_decorator
 from erp.models import Category
 
 # Vista basada en una función
@@ -15,6 +18,14 @@ def category_list(request):
 class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {'name': 'Juanito'}
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
